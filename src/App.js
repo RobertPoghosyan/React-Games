5f847528907372d95fc1acb303eb708d56706447
RobertPoghosyan <[robertpoghosian@gmail.com]>
2021-01-30 17:15:05 +0400

    Building carcass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
+
+import Header from "./containers/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./containers/Footer/Footer";
 
 import './App.css';
+import Homepage from "./containers/Homepage/Homepage";
 
 function App() {
+  const [score,setScore]=useState(0);
   return (
     <div className="App">
-      HELLO
+      <Header score = {score}/>
+      <Layout>
+        <Homepage/>
+      </Layout>
+      <Footer/>
     </div>
   );
 }
---
+++ src/data-mockup/image-mockup.js
@@ -0,0 +1,24 @@
+import paperImg from "../assets/paper.svg";
+import rockImg from "../assets/rock.svg";
+import scissorsImg from "../assets/scissors.svg";
+
+
+export const images = [
+    {
+        id:1,
+        imgSrc:paperImg,
+        alt:"paper"
+    },
+
+    {
+        id:2,
+        imgSrc:rockImg,
+        alt:"rock"
+    },
+
+    {
+        id:3,
+        imgSrc:scissorsImg,
+        alt:"scissors"
+    }
+];
